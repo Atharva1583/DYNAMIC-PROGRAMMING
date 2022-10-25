49afd044198dba992aa5ec09d90ecbadfdbd2c01
Atharva Joshi <86041881+Atharva1583@users.noreply.github.com>
2022-10-26 00:32:00 +0530

    Create space optimized.cpp

---
+++ Striver's DP Series Problems/Unique Paths/space optimized.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+
+int uniquePaths(int m, int n) {
+    vector<int> dp(n,0);
+    for(int i=0;i<m;i++){
+        vector<int> temp(n,0);
+        for(int j=0;j<n;j++){
+           if(i==0 && j==0){
+               temp[0]=1;
+           }else{
+               temp[j]=dp[j]+temp[j-1];
+           }
+        }
+        dp=temp;
+    }
+    return dp[n-1];
+    
+}
